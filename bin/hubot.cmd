64ea550489c5ea384accaefa6470f5bf29c6eeba
Tim <wayfarer1313@gmail.com>
2019-11-23 19:11:02 -0600

    2nd generation

--- bin/hubot.cmd
+++ bin/hubot.cmd
@@ -4,4 +4,4 @@ call npm install
 SETLOCAL
 SET PATH=node_modules\.bin;node_modules\hubot\node_modules\.bin;%PATH%
 
-node_modules\.bin\hubot.cmd --name "charlotte-a-cavatica" %* 
+node_modules\.bin\hubot.cmd --name "charlie" %* 
